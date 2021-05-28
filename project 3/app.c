d288efba7bfb970c05812263b4022f970f2fd4d4
Ufuk Bombar <ufuk.bombar@bordatech.com>
2021-05-28 11:40:48 +0300

    Implement allocation algorithm and test

--- project 3/app.c
+++ project 3/app.c
@@ -24,47 +24,22 @@ int main()
     r = sbmem_close();
     printf("sbmem_close() => %d\n", r);
 
-    pid_t pid = fork();
+    sbmem_alloc(256);
+    sbmem_alloc(512);
+    sbmem_alloc(512);
 
-    if (pid != 0) {
-        void* p = sbmem_alloc(128);
-        printf("p1: sbmem_alloc(128) => %p\n", p);
-        sbmem_free(p);
+    // pid_t pid = fork();
 
-    } else {
-        void* p = sbmem_alloc(512);
-        printf("p2: sbmem_alloc(512) => %p\n", p);
-        sbmem_free(p);
-    }
+    // if (pid != 0) {
+    //     void* p = sbmem_alloc(128);
+    //     printf("p1: sbmem_alloc(128) => %p\n", p);
+    //     sbmem_free(p);
 
+    // } else {
+    //     void* p = sbmem_alloc(512);
+    //     printf("p2: sbmem_alloc(512) => %p\n", p);
+    //     sbmem_free(p);
+    // }
 
-
-
-    // if (ret == -1)
-    //     exit(1);
-
-    // printf("Allocate 512 bytes:   ");
-    // char *a = sbmem_alloc(512);
-    // printf("Allocate 32 bytes:    ");
-    // void *b = sbmem_alloc(32);
-    // printf("Allocate 8 bytes:     ");
-    // void *c = sbmem_alloc(8);
-
-    // // allocate space to for characters
-    // for (int i = 0; i < ASIZE; ++i)
-    //     a[i] = 'a'; // init all chars to ‘a’
-
-    // printf("Deallocate 512 bytes: ");
-    // sbmem_free(c);
-    // printf("Deallocate 32 bytes:  ");
-    // sbmem_free(b);
-    // printf("Deallocate 8 bytes:   ");
-    // sbmem_free(a);
-
-    // printf("\n");
-
-    // sbmem_close();
-
-    // sbmem_remove();
-    // return (0);
+    return 0;
 }
--- project 3/sbmemlib.c
+++ project 3/sbmemlib.c
@@ -49,6 +49,68 @@ int heap_right(int c) {
     return 2 * c + 2;
 }
 
+// allocate memory on bitmap, size_pow2 is power of two!
+// return the heap index of the allocated chunk
+int __allocate_on_bitmap(int size_pow2, int heap_index, int depth) {
+    int current_size = segment_size >> depth;
+
+    // // already allocated! Cannot allocate // NOT CORRECT!
+    // if (freelist[heap_index] == 1) {
+    //     return -1;
+    // }
+
+    if (current_size == size_pow2) {
+        // Test if the hole is empty!
+
+        if (freelist[heap_index] == 1) {
+            return -1;
+        }
+
+        freelist[heap_index] = 1;
+        return heap_index;
+    } else if (current_size > size_pow2) {
+        int h_left = heap_left(heap_index);
+        int h_right = heap_right(heap_index);
+
+        int left_allocation_index = __allocate_on_bitmap(size_pow2, h_left, depth + 1);
+
+        if (left_allocation_index != -1) {
+            freelist[heap_index] = 1;
+            return left_allocation_index;
+        }
+
+        int right_allocation_index = __allocate_on_bitmap(size_pow2, h_right, depth + 1);
+
+        if (right_allocation_index != -1) {
+            freelist[heap_index] = 1;
+            return right_allocation_index;
+        }
+
+        return -1; // not enough memory!
+    } else {
+        return -1; // not enough memory!
+    }
+}
+
+int __max(int a, int b) {
+    return a > b ? a : b;
+}
+
+int __print_heap() {
+    int next_pow2 = 1;
+
+    for (size_t i = 0; i < freelist_size; i++) {
+        if (i == next_pow2 - 1) {
+            printf("\n");
+            next_pow2 = next_pow2 << 1;
+        }
+
+        printf("%d ", freelist[i]);
+    }
+
+    printf("\n");
+}
+
 // This will be initialized by one process thus no need for semaphores.
 int sbmem_init(int segsize)
 {
@@ -83,9 +145,9 @@ int sbmem_init(int segsize)
     }
 
     // allocate the free list heap inside the memory segment
-    int free_list_size = sizeof(char) * (2 * (segsize_c / min_memory_size) - 1);
+    int free_list_size = (2 * (segsize_c / min_memory_size) - 1);
 
-    int r = ftruncate(fd, free_list_size + segsize_c);
+    int r = ftruncate(fd, sizeof(char) * free_list_size + segsize_c);
     
     // If cannot truncate
     if (r == -1) {
@@ -148,6 +210,23 @@ int sbmem_close()
 void *sbmem_alloc(int reqsize)
 {
     sem_wait(&mutex);
+
+    printf("-----------------------\n");
+
+    __print_heap();
+
+    reqsize = __max(to_multiple_of_two(reqsize), (0x01 << MIN_MEMORY_POW)); // cannot allocate less than 128 bytes!
+
+    printf("req size = %d\n", reqsize);
+
+    int heap_index = __allocate_on_bitmap(reqsize, 0, 0);
+
+    printf("> allocation result = %d\n", heap_index);
+    
+
+    __print_heap();
+
+    printf("-----------------------\n");
 
     sem_post(&mutex);
 
